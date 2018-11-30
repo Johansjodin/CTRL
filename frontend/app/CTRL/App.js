33cdfcce913addce847238112b3d5e6bfcd23442
Johansjodin <johan.sjodin@hotmail.com>
2018-11-30 10:00:15 +0100

    initial commit

---
+++ frontend/app/CTRL/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import LoginScreen from './views/loginScreen';
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<LoginScreen />
+		);
+	}
+}
+
+
+
+/*<View style={styles.formWrapper}>
+		  <Text>hej3</Text>
+		  <Button
+			onPress={() => {}}
+			title="Press me"
+			color="#0000ff"
+			accessibilityLabel="Learn more about this purple button"
+		  />
+		</View>*/
